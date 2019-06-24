f0b98d94e046ae81bd69a0f521ef4ee1fd864bef
Greg <Greg@DESKTOP-3OLTALC>
2019-06-24 11:18:44 +0100

    EstateTest Tests constructors getEstateSize, nodeInteract.

---
+++ test/model/EstateTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Greg
+ */
+public class EstateTest {
+    
+    public EstateTest() {
+    }
+    
+    @Test
+    public void constructorTest() {
+        Estate estate = new Estate();
+        estate = new Estate(100,100);
+    }
+    
+    @Test
+    public void getEstateSizeTest() {
+        System.out.println("Creating an estate of 10 honest and 10 deceptive nodes.");
+        Estate estate = new Estate(10,10);
+        System.out.println("The reported size is: " + estate.getEstateSize() + " nodes");
+    }
+    
+    @Test
+    public void nodeInteractTest() {
+        Estate estate = new Estate();
+        System.out.println("The reported size is: " + estate.getEstateSize() + " nodes");
+        LogArray logArray  = new LogArray();
+        estate.nodeInteract(0, logArray);
+    }
+}
