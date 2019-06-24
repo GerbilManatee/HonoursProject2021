3cd60cfae1c58c18e0d5d30f3ab4b37773dc710a
Greg <Greg@DESKTOP-3OLTALC>
2019-06-24 15:55:15 +0100

    Added interpretresults for a specified range of steps in the run.

--- src/model/LogInterpreter.java
+++ src/model/LogInterpreter.java
@@ -88,4 +88,50 @@ public class LogInterpreter {
         return ((sum/interceptResults.length)*100);
     }
     
+    public int[] getInterceptResults(int startStep, int endStep) {
+        //First check the size of the LogArray
+        int logArraySize = logArray.getLogArraySize();
+        //Then set up an array of the correct size to contain all the results.
+        int[] output = new int[logArraySize];
+        //Now we iterate through all the ResultLogs
+        for(int logNumber = 0; logNumber < logArraySize; logNumber++) {
+            /*
+            In each loop, first get the size of the ResultLog.  Never mind that 
+            they're likely all the same, that might not always be the case.
+            */
+            int resultLogSize = logArray.getResultLogLength(logNumber);
+            //create a boolean to record whether the run involves an interaction
+            //with a deceptive Node.
+            boolean hitDeception = false;
+            //Use a while loop to pull out of the check once we have a confirmed hit.
+            //Could dump this if it's inelegant, but for huge logs it might save time.
+            while(hitDeception == false){
+                //Default output[logNumber to 0:
+                output[logNumber] = 0;
+                //Then iterate through the interactions, looking for a hit.
+                for(int interaction = startStep;((interaction < resultLogSize) && (interaction < endStep)); interaction++) {
+                    /*
+                    change hitDeception to true when you find an interaction 
+                    with a deceptive node - it goes to false again in the 
+                    */
+                    hitDeception = logArray.getResult(logNumber, interaction);
+                    //Then, if it's true and you're about to fall out of the while loop:
+                    if(hitDeception == true){
+                        output[logNumber] = 1;
+                    }
+                }
+                //break if hitDeception never becomes true
+                break;
+            }
+                
+        }
+        //return the output array.
+        return output;
+        
+    }
+
+    
+    
+    
+    //private int getInterceptCount()
 }
